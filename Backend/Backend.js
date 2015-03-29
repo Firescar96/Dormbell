6f9d2151fe704ad2023492b8ba1a5e19b86a6101
Firescar96 <nchinda2@gmail.com>
2015-03-29 17:44:09 -0400

    backend should pass any rings to it

--- Backend/Backend.js
+++ Backend/Backend.js
@@ -24,112 +24,84 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
 
-Users = new Meteor.Collection('users');
-Events = new Meteor.Collection('events');
+	Users = new Meteor.Collection('users');
+	Events = new Meteor.Collection('events');
 
-Router.map(function () {
-  this.route("/", {
-    where: "server",
-    action: function(){
-      console.log('################################################');
-      //console.log(this.request.method);
-      //console.log(this.request.headers);
+	Router.map(function () {
+		this.route("/", {
+	    		where: "server",
+	    		action: function(){
+	      		console.log('################################################');
+			      //console.log(this.request.method);
+			      //console.log(this.request.headers);
 
-      //console.log('------------------------------');
-      console.log(this.request.body);
-      //console.log('------------------------------');
+			      //console.log('------------------------------');
+			      console.log(this.request.body);
+			      //console.log('------------------------------');
 
-      this.response.statusCode = 200;
-      this.response.setHeader("Content-Type", "application/json");
-      this.response.setHeader("Access-Control-Allow-Origin", "*");
-      this.response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+			      this.response.statusCode = 200;
+			      this.response.setHeader("Content-Type", "application/json");
+			      this.response.setHeader("Access-Control-Allow-Origin", "*");
+			      this.response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-      if (this.request.method == 'POST') {
-	HandleData(this.request.body);
-	this.response.writeHead(200, {'Content-Type': 'text/plain'});
-	this.response.end("");
-      }
+		     		if (this.request.method == 'POST') {
+					HandleData(this.request.body);
+					this.response.writeHead(200, {'Content-Type': 'text/plain'});
+					this.response.end("");
+		      	}
 
-      if (this.request.method == 'GET') {
-      	/*Collection("REST")
-
-			Meteor.publish("RESTS", function(){
-			 rests.find();
-			});
-			
-			rests.remove({}); // remove all
-			rests.insert({message: "Some message to show on every client."});
-      		console.log("gotten");*/
-      	if(this.request.query.checkName != null)
-      	{
-      		console.log(this.request.query.checkName);
-      		var resUsr = Users.findOne({name:this.request.query.checkName});
-      		var exists = resUsr != undefined;
-      		this.response.writeHead(200, {'Content-Type': 'text/plain'});
-      		
-				if(this.request.query.regId == null && exists)
-					this.response.end("true")
-				if(this.request.query.regId == null && !exists)
-					this.response.end("false")
-				else if(exists && resUsr.regId == this.request.query.regId) //return true if matching user found, or if the requesting user is in the database
-					this.response.end("true");
-				else if(!exists)
-					this.response.end("true");
-      		else
-      			this.response.end("false");
-			}      
-      }
-    }
-  });
-});
+		      	if (this.request.method == 'GET') {
+			      	if(this.request.query.checkName != null)
+			      	{
+			      		console.log(this.request.query.checkName);
+			      		var resUsr = Users.findOne({username:this.request.query.checkName});
+			      		var exists = resUsr != undefined;
+			      		this.response.writeHead(200, {'Content-Type': 'text/plain'});
+			      		if(this.request.query.regId != null)
+			      		{
+			      			if(exists && resUsr.regId == this.request.query.regId) //return true if matching user found, or if the requesting user is in the database
+								this.response.end("true");
+							else
+								this.response.end("false");
+						}
+						else
+							this.response.end(exists.toString());
+					}      
+	      		}
+	    		}
+	  	});
+	});
 
 		
-var apiKey = 'AIzaSyAk_PxK_3WfDeFQOL0fDpPpqaA5scekrEk'; //TODO: Make this a legit apikey
-var gcm = Meteor.npmRequire('node-gcm');
-			
-var HandleData = function(query)
-{
-	//Users.remove({});
-	if(query.location != undefined)
-	{
-		Users.update({name: query.host}, {$set: {location: query.location}});	
-		console.log("Updated user: " + query.host + " location");
-		//console.log(Users.find({}).fetch());
-		return;
-	}	
+	var gcm = Meteor.npmRequire('node-gcm');
+	var sender = new gcm.Sender('AIzaSyAk_PxK_3WfDeFQOL0fDpPpqaA5scekrEk');//TODO: Make this a legit apikey
 
-	if(query.host != undefined && query.regId != undefined) {
-		nameUser = Users.findOne({name:query.host});
-		regIdUser = Users.findOne({name:query.regId});
-		if (nameUser == undefined && regIdUser == undefined)
-				Users.insert({name: query.host, regId: query.regId});	
-		else if (nameUser == undefined)
-			if(regIdUser != undefined)
-				Users.update({regId: query.regId}, {$set: {name: query.host}});
-		else if (regIdUser == undefined)
-			if(nameUser != undefined)
-				Users.update({name: query.host}, {$set: {regId: query.regId}});
-		return;
-	}
-	if(query.event != undefined)
+	var HandleData = function(query)
 	{
-		if (query.event.privacy == "open") 
+		//Users.remove({}); Should this line even exist?
+		if(query.location != undefined)
 		{
-			var toUsr = Users.find({}).fetch();
-		} else if(query.chat == "true")
-		{
-			var toUsr = Users.find({}).fetch();
-		} else	
-		{
-			var nxtUsr = query.to.split(',');
-			//console.log(nxtUsr);
-			var toUsr = [];
-			for(var i in nxtUsr)
-			{
-			//console.log(nxtUsr[i]);
-				toUsr.push(Users.findOne({name:nxtUsr[i]}));
-			}
+			Users.update({username: query.username}, {$set: {location: query.location}});	
+			console.log("Updated user: " + query.username + " location");
+			//console.log(Users.find({}).fetch());
+			return;
+		}	
+
+		if(query.username != undefined && query.regId != undefined) {
+			nameUser = Users.findOne({username:query.username});
+			regIdUser = Users.findOne({username:query.regId});
+			if (nameUser == undefined && regIdUser == undefined)
+					Users.insert({username: query.username, regId: query.regId});	
+			else if (nameUser == undefined)
+				if(regIdUser != undefined)
+					Users.update({regId: query.regId}, {$set: {username: query.username}});
+			else if (regIdUser == undefined)
+				if(nameUser != undefined)
+					Users.update({username: query.username}, {$set: {regId: query.regId}});
+			return;
 		}
+
+		var toUsr = Users.find({lock:query.lock}).fetch();
 		
 		/*if(Events.findOne({hash:query.event.hash}) == undefined) //TODO: save events to be sent when a user reconnets
 		{
@@ -141,84 +113,40 @@ var HandleData = function(query)
 	    		host:query.event.host
 		    });
 		}*/
-	}
-	else
-	{
-		var toUsr = Users.find({}).fetch();
 
-		toUsr.push(Users.findOne({name:query.to}));
-	}	
-
-	console.log(toUsr)	
-	for(var i in toUsr)
-	{
-		console.log(toUsr[i]);
-		if (toUsr[i] == null) 
-			continue;
-		
-		if(query.event != undefined)
-			var hostUsr = Users.findOne({name:query.event.host});
-		else 
-			var hostUsr = Users.findOne({name:query.host});
-		//console.log(query.event.host);
-		//console.log(toUsr[i].location);
-		//console.log(Users.findOne({name:query.event.host}));
-		if(toUsr[i].location.latitude != undefined)
+		console.log(toUsr)	
+		var registrationIds = [];
+		for(var i in toUsr)
 		{
-			toUsr[i].location.latitude;
-			var latPow = Math.pow(parseInt(hostUsr.location.latitude)-parseInt(toUsr[i].location.latitude),2);
-			var longPow = Math.pow(parseInt(hostUsr.location.longitude)-parseInt(toUsr[i].location.longitude),2);
-			var dist = Math.sqrt(latPow+longPow);
-			if(dist > 1)
+			console.log(toUsr[i]);
+			if (toUsr[i] == null) 
 				continue;
-		}	
 			
-		if(query.chat == "true")
-			var mess = {
-		   	registration_id: toUsr[i].regId , // required
-		    	"data.chat": "{\
-		    		message:\""+query.message+"\",\
-		    		author:\""+query.author+"\",\
-		    		date:\""+query.date+"\",\
-		    		location:\""+query.location+"\",\
-				hash:\""+query.hash+"\",\
-		    		host:\""+query.host+"\"\
-		    	}"
-			};
-	
-		if(toUsr[i].regId && query.event != null)
-			var mess = {
-		   	registration_id: toUsr[i].regId , // required
-		    	"data.event": "{\
-		    		privacy:\""+query.event.privacy+"\",\
-		    		location:\""+query.event.location+"\",\
-		    		date:\""+query.event.date+"\",\
-		    		hash:\""+query.event.hash+"\",\
-		    		host:\""+query.event.host+"\"\
-		    	}"
-			};
+			ivar hostUsr = Users.findOne({username:query.username});
+			//console.log(toUsr[i].location);
+			//console.log(Users.findOne({username:query.event.host}));
+			if(toUsr[i].location.latitude != undefined)
+			{
+				var latPow = Math.pow(parseInt(hostUsr.location.latitude)-parseInt(toUsr[i].location.latitude),2);
+				var longPow = Math.pow(parseInt(hostUsr.location.longitude)-parseInt(toUsr[i].location.longitude),2);
+				var dist = Math.sqrt(latPow+longPow);
+				if(dist > 1)
+					continue;
+			}	
+			
+			registrationIds.push(toUsr[i].regId);
+		}
+
+		var message = new  gcm.Message();
+		if(query.ring == true)
+		{
+			message.sender = "\""query.sender"\"",
+			message.lock="\""query.lock"\""
+		}
 		
-		/*if(toUsr[i].regId && query.hash!= null) {
-			var curEve = Events.findOne({hash:query.hash});
-			var mess = {
-		   	registration_id: toUsr[i].regId , // required
-		    	"data.event": "{\
-		    		privacy:\""+curEve.privacy+"\",\
-		    		location:\""+curEve.location+"\",\
-		    		date:\""+curEve.date+"\",\
-		    		hash:\""+curEve.hash+"\",\
-		    		host:\""+curEve.host+"\"\
-		    	}"
-			};
-		}*/
-		
-			gcm.send(mess, function(err, messageId){
-		    	if (err) {
-		    		console.log("Something has gone wrong!");
-		    	} else {
-		        	console.log("Sent with message ID: ", messageId);
-		    	}
-			});
+		sender.send(message, registrationIds, 10, function (err, result) {
+	  		if(err) console.error(err);
+	  		else    console.log(result);
+		});
 	}
-}
 }
